845d594fe79af725ed07863af4478812e0541907
MacanitaDB <105833395+Mcanita@users.noreply.github.com>
2024-11-07 00:09:57 -0300

    Se agregó los métodos para mostrar, agregar y eliminar Categoría

--- TPFinal_Ecommerce_Grupo14B/Negocio/CategoriaNegocio.cs
+++ TPFinal_Ecommerce_Grupo14B/Negocio/CategoriaNegocio.cs
@@ -40,5 +40,90 @@ namespace Negocio
             }
             return lista;
         }
+
+        public void agregar(Categoria categoria)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT COUNT(*) FROM Categorias WHERE nombre = @nombre");
+                datos.setearParametro("@nombre", categoria.Nombre);
+
+                int cantidad = (int)datos.ejecutarAccionScalar();
+
+                if(cantidad > 0)
+                {
+                    throw new Exception($"Ya existe una Categoría con nombre '{categoria.Nombre}' en la base de datos. ");
+                }
+                else
+                {
+                    datos.setearConsulta("INSERT INTO Categorias (nombre) VALUES (@nombre)");
+                    datos.setearParametro("@nombre", categoria.Nombre);
+                    datos.ejecutarAccion();
+                }
+            }
+            catch (Exception ex )
+            {
+
+                throw new Exception("Error al agregar la Categoría : " + ex.Message) ;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            try
+            {
+                AccesoDatos datos = new AccesoDatos();
+                datos.setearConsulta(" DELETE FROM Categorias WHERE idCategoria = @id");
+                datos.setearParametro("id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        public void modificar (Categoria categoria)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT COUNT (*) FROM Categorias WHERE nombre = @nombre AND idCategoria <> @id");
+                datos.setearParametro("@nombre", categoria.Nombre);
+                datos.setearParametro("@id", categoria.Id);
+
+                int cantidad = (int)datos.ejecutarAccionScalar();
+
+                if(cantidad > 0)
+                {
+                    throw new Exception($"Ya existe una Categoria con el nombre '{categoria.Nombre}' en la base de datos. ");
+                }
+                else
+                {
+                    datos.setearConsulta("UPDATE Categorias SET nombre = @nombre WHERE idCategoria = @id");
+                    datos.setearParametro("@nombre", categoria.Nombre);
+                    datos.setearParametro("@id", categoria.Id);
+                    datos.ejecutarAccion();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error al modificar la Categoria " + ex.Message) ;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
